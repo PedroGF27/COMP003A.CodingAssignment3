5488f78a44fe8513f53286cb2ba1fc3dafba1e7a
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-01 18:37:37 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Pedro Garcia
+//COMP003A
+//Johnathan Cruz
+//Lecture Activity 3-3
+namespace COMP003A.CodingAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
